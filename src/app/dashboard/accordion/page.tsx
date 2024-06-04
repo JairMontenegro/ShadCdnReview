9c4fd3f8dcd84c07ed9b93a5e7d96f3d3b3177fa
jair <jairmontenegro@flare.com.co>
2024-06-04 16:28:05 -0500

    feat: accordion component added

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,38 @@
-export default function Page() {
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "The capital of France is Paris.",
+  },
+  {
+    id: 2,
+    question: "How many continents are there?",
+    answer:
+      "There are seven continents: Africa, Antarctica, Asia, Europe, North America, Australia, and South America.",
+  },
+  {
+    id: 3,
+    question: "What is the tallest mountain in the world?",
+    answer: "The tallest mountain in the world is Mount Everest.",
+  },
+];
+
+export default function AccordionPage() {
   return (
-    <div>
-      <h1>Accordeon page</h1>
-    </div>
+    <Accordion type="single" className="w-full">
+      {items.map((item) => (
+        <AccordionItem value={`${item.id}`} key={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
